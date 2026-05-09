42781d6882feccee787df160ce4caa97e8200c27
LinggarRam <123456789+odin@users.noreply.github.com>
2026-05-10 00:03:00 +0700

    finally project

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,48 @@
 import { useState } from "react";
+import Board from "./components/Board";
 
-function Square() {
-  const [value, setValue] = useState("");
+export default function Game() {
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const [currentMove, setCurrentMove] = useState(0);
+  const xIsNext = currentMove % 2 === 0;
+  const currentSquares = history[currentMove];
 
-  function handleClick() {
-    setValue("X");
+  function jumpTo(nextMove) {
+    setCurrentMove(nextMove);
   }
 
-  return (
-    <button className="square" onClick={handleClick}>
-      {value}
-    </button>
-  );
-}
+  function handlePlay(nextSquares) {
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1);
+  }
 
-export default function board() {
+  const moves = history.map((squares, move) => {
+    let desc;
+    if (move > 0) {
+      desc = "Go to move #" + move;
+    } else {
+      desc = "Go to start";
+    }
+    return (
+      <li key={move}>
+        <button onClick={() => jumpTo(move)}>{desc}</button>
+      </li>
+    );
+  });
+
   return (
-    <div className="main-content">
-      <div className="board-container">
-        <div className="board">
-          <Square />
-          <Square />
-          <Square />
-          <Square />
-          <Square />
-          <Square />
-          <Square />
-          <Square />
-          <Square />
+    <div className="game-wrapper">
+      <div className="game">
+        <div className="game-board">
+          <Board
+            xIsNext={xIsNext}
+            squares={currentSquares}
+            onPlay={handlePlay}
+          />
+        </div>
+        <div className="game-info">
+          <ol>{moves}</ol>
         </div>
       </div>
       <footer>
